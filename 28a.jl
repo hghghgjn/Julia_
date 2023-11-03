f3734affac3c7d221372b876f598a79e29ea4606
hghghgjn <91079751+hghghgjn@users.noreply.github.com>
2023-11-03 11:30:10 +0300

    Create 28a.jl

---
+++ 28a.jl
@@ -0,0 +1,10 @@
+function fib_norecurs(n)
+    F_prev = F_next = 1
+    while n > 1
+        F_next, F_prev = F_prev + F_next, F_next
+        n -= 1
+    end
+    return F_prev
+end
+
+println(fib_norecurs(10))
